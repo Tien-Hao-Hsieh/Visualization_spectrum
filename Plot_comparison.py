eac4f0d44be8d31acaf967a2df2de162829c2969
Tien-Hao Hsieh <thhsieh@mpe.mpg.de>
2021-02-24 18:39:37 +0100

    Update

--- Plot_comparison.py
+++ Plot_comparison.py
@@ -16,44 +16,37 @@ class Cursor():
     def mouse_move(self, event):
         if not event.inaxes:
             return
-        x, y = event.xdata, event.ydata
-	ax2=axes([0.55,0.05,0.45,0.4])
-	ax2.cla()
-	ax2.set_xlim(-10,30)
-	spe=data[:,int(y),int(x)]
-	ax2.step(Vel_li_nh3,spe,c='#8E8E8E',lw=2,where='mid')
-	popt=Fit_result[int(y),int(x)]
-	error=Fit_result_err[int(y),int(x)]
-	ax2.plot(v_li,nh3_fun.Tmb_all(v_li,popt[0],popt[1],popt[2],popt[3]),c='r',lw=1)
-	ax2.annotate(r'$T_{\rm ex}$  $=$'+'%.1f'%popt[0]+'$\pm$'+'%.1f'%error[0]+r'$K$',xycoords='axes fraction',xy=(0.05,0.92),fontsize=10)
-	ax2.annotate(r'$\tau$     $=$'+'%.2f'%popt[1]+'$\pm$'+'%.2f'%error[1],xycoords='axes fraction',xy=(0.05,0.85),fontsize=10)
-	ax2.annotate(r'$V_{\rm LSR}=$'+'%.2f'%popt[2]+'$\pm$'+'%.2f'%error[2]+r'km s$^{-1}$',xycoords='axes fraction',xy=(0.05,0.78),fontsize=10)
-	ax2.annotate(r'$\Delta V$  $=$'+'%.2f'%(popt[3]*fac)+'$\pm$'+'%.2f'%(error[3]*fac)+r'km s$^{-1}$',xycoords='axes fraction',xy=(0.05,0.71),fontsize=10)
-
-	ax3=axes([0.55,0.55,0.45,0.4])
-	ax3.cla()
-	ax3.set_xlim(-10,30)
-	spe=data2[:,int(y),int(x)]
-	ax3.step(Vel_li_n2hp,spe,c='b',lw=2,where='mid')
-	popt=Fit_result2[int(y),int(x)]
-	error=Fit_result_err2[int(y),int(x)]
-	ax3.plot(v_li,n2hp_fun.Tmb_all(v_li,popt[0],popt[1],popt[2],popt[3]),c='r',lw=1)
-	ax3.annotate(r'$T_{\rm ex}$  $=$'+'%.1f'%popt[0]+'$\pm$'+'%.1f'%error[0]+r'$K$',xycoords='axes fraction',xy=(0.05,0.92),fontsize=10)
-	ax3.annotate(r'$\tau$     $=$'+'%.2f'%popt[1]+'$\pm$'+'%.2f'%error[1],xycoords='axes fraction',xy=(0.05,0.85),fontsize=10)
-	ax3.annotate(r'$V_{\rm LSR}=$'+'%.2f'%popt[2]+'$\pm$'+'%.2f'%error[2]+r'km s$^{-1}$',xycoords='axes fraction',xy=(0.05,0.78),fontsize=10)
-	ax3.annotate(r'$\Delta V$  $=$'+'%.2f'%(popt[3]*fac)+'$\pm$'+'%.2f'%(error[3]*fac)+r'km s$^{-1}$',xycoords='axes fraction',xy=(0.05,0.71),fontsize=10)
+        ra, dec = event.xdata, event.ydata
+	print(x,y)
+#	ax2=axes([0.55,0.05,0.45,0.4])
+#	ax2.cla()
+#	ax2.set_xlim(-10,30)
+#	spe=data[:,int(y),int(x)]
+#	ax2.step(Vel_li_nh3,spe,c='#8E8E8E',lw=2,where='mid')
+#	popt=Fit_result[int(y),int(x)]
+#	error=Fit_result_err[int(y),int(x)]
+#	ax2.plot(v_li,nh3_fun.Tmb_all(v_li,popt[0],popt[1],popt[2],popt[3]),c='r',lw=1)
+#	ax2.annotate(r'$T_{\rm ex}$  $=$'+'%.1f'%popt[0]+'$\pm$'+'%.1f'%error[0]+r'$K$',xycoords='axes fraction',xy=(0.05,0.92),fontsize=10)
+#	ax2.annotate(r'$\tau$     $=$'+'%.2f'%popt[1]+'$\pm$'+'%.2f'%error[1],xycoords='axes fraction',xy=(0.05,0.85),fontsize=10)
+#	ax2.annotate(r'$V_{\rm LSR}=$'+'%.2f'%popt[2]+'$\pm$'+'%.2f'%error[2]+r'km s$^{-1}$',xycoords='axes fraction',xy=(0.05,0.78),fontsize=10)
+#	ax2.annotate(r'$\Delta V$  $=$'+'%.2f'%(popt[3]*fac)+'$\pm$'+'%.2f'%(error[3]*fac)+r'km s$^{-1}$',xycoords='axes fraction',xy=(0.05,0.71),fontsize=10)
+#
+#	ax3=axes([0.55,0.55,0.45,0.4])
+#	ax3.cla()
+#	ax3.set_xlim(-10,30)
+#	spe=data2[:,int(y),int(x)]
+#	ax3.step(Vel_li_n2hp,spe,c='b',lw=2,where='mid')
+#	popt=Fit_result2[int(y),int(x)]
+#	error=Fit_result_err2[int(y),int(x)]
+#	ax3.plot(v_li,n2hp_fun.Tmb_all(v_li,popt[0],popt[1],popt[2],popt[3]),c='r',lw=1)
+#	ax3.annotate(r'$T_{\rm ex}$  $=$'+'%.1f'%popt[0]+'$\pm$'+'%.1f'%error[0]+r'$K$',xycoords='axes fraction',xy=(0.05,0.92),fontsize=10)
+#	ax3.annotate(r'$\tau$     $=$'+'%.2f'%popt[1]+'$\pm$'+'%.2f'%error[1],xycoords='axes fraction',xy=(0.05,0.85),fontsize=10)
+#	ax3.annotate(r'$V_{\rm LSR}=$'+'%.2f'%popt[2]+'$\pm$'+'%.2f'%error[2]+r'km s$^{-1}$',xycoords='axes fraction',xy=(0.05,0.78),fontsize=10)
+#	ax3.annotate(r'$\Delta V$  $=$'+'%.2f'%(popt[3]*fac)+'$\pm$'+'%.2f'%(error[3]*fac)+r'km s$^{-1}$',xycoords='axes fraction',xy=(0.05,0.71),fontsize=10)
 
         plt.draw()
 
 ############################################### NH3 ########################################
-#mom0=nan_to_num(pyfits.getdata('../NH3/GBT_Friesen/Moment0/IRAS16253_mom0.fits')[0][0])
-mom0=nan_to_num(pyfits.getdata('Data/N2H+/Tobin_IRAS16253_data/HFS_fitting/DCE185_HFS_vlsr.fits')[0][0])
-noi=loadtxt('Data/NH3/GBT_Friesen/Moment0/Noise/Noise_table',usecols=[1])
-SN=7
-Mask=zeros(shape(mom0))
-Mask[50:250,50:250]=1
-Mask[mom0<SN*noi]=0
-
 FITS='Data/NH3/GBT_Friesen/To_brightness_temperature/GBT100m.Tbrightness.fits'
 fits=pyfits.open(FITS)
 data=fits[0].data
@@ -86,13 +79,13 @@ Fit_result2=load('Data/N2H+/Tobin_IRAS16253_data/HFS_fitting/Output_HFSfitting.n
 Fit_result_err2=load('Data/N2H+/Tobin_IRAS16253_data/HFS_fitting/Output_HFSfitting_err.npy')
 ############################################### N2H #########################################
  
-                                                                          
-                                                                           
+image=imread('Data/IR_images/IRAS16253_IR.png')
+
 v_li=arange(-30,30,0.005)                                    
 fig=figure(figsize=(10,5))                                                 
-ax=axes([0.07,0.1,0.45,0.9])                                               
-ax.imshow(mom0,interpolation=None,origin='lower')
-ax.contour(Mask,levels=[0.5])
+ax=axes([0.05,0.05,0.45,0.85])
+ax.imshow(image,interpolation=None,extent=(247.11717,247.06250,-24.65000,-24.58333),aspect='equal')
+ax.tick_params(labelleft=False,labelbottom=False,direction='in')
 
 cursor = Cursor(ax)
 connect('motion_notify_event', cursor.mouse_move)
